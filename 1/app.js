a9fbbefe63ea2ed800a1153021428d1b259aea23
mousebaiker <mousebaiker@gmail.com>
2017-03-10 17:44:46 +0300

    Update app.js

--- 1/app.js
+++ 1/app.js
@@ -5,13 +5,13 @@ var first_sorted = first_string.split("").sort();
 var second_sorted = second_string.split("").sort();
 check(first_sorted, second_sorted);
 function check(first_sorted, second_sorted) {
-  for (int i = 0; i < first_sorted.length(); ++i) {
+  for (int i = 0; i < first_sorted.length; ++i) {
     if (first_sorted[i] != second_sorted[i]) {
         alert("Strings are not anagrams.");
       return;
     }
   }
-  if (first_string.length()===second_string.lenght()){
+  if (first_string.length===second_string.lenght){
     alert("Strings are anagrams.");
   }
   else {
